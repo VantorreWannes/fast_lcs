0c7b41be22c7cef5096b2ad921597a1aed0a97aa
Navoso <VantorreWannes@gmail.com>
2024-01-09 15:17:44 +0100

    Implement Alcs.subsequence()

--- src/new_algos/counts_lcs.rs
+++ src/new_algos/counts_lcs.rs
@@ -20,17 +20,36 @@ impl<'a> Alcs<'a> {
         }
     }
 
-    pub fn is_empty(&self) -> bool {
+    pub fn is_empty(self) -> bool {
         self.len() == 0
     }
 
-    pub fn len(&self) -> usize {
+    pub fn len(self) -> usize {
         self.subsequence().len()
     }
 
-    pub fn subsequence(&self) -> Vec<u8> {
-        todo!();
+    pub fn subsequence(mut self) -> Vec<u8> {
+        let mut last_lcs_indexes: (usize, usize) = (0, 0);
+        let mut lcs: Vec<u8> = vec![];
+        while let Some((source_offset, target_offset)) = self.next_pair_offsets(
+            &self.source[last_lcs_indexes.0..],
+            &self.target[last_lcs_indexes.1..],
+        ) {
+            for source_num in &self.source[last_lcs_indexes.0..=last_lcs_indexes.0+source_offset] {
+                self.source_counts[*source_num as usize] -= 1;
+            }
+            for target_num in &self.target[last_lcs_indexes.1..=last_lcs_indexes.1+target_offset] {
+                self.target_counts[*target_num as usize] -= 1;
+            }
+            last_lcs_indexes = (
+                last_lcs_indexes.0 + source_offset + 1,
+                last_lcs_indexes.1 + target_offset + 1,
+            );
+            lcs.push(self.source[last_lcs_indexes.0 - 1]);
+        }
+        lcs
     }
+
 
     fn next_pair_offsets(&self, source: &'a [u8], target: &'a [u8]) -> Option<(usize, usize)> {
         let source_len = source.len();
@@ -50,8 +69,8 @@ impl<'a> Alcs<'a> {
                     .iter()
                     .map(|target_num| self.target_counts[*target_num as usize])
                     .collect();
-                let new_pair_scores =
-                    Alcs::pair_counts_score(&source_counts) + Alcs::pair_counts_score(&target_counts);
+                let new_pair_scores = Alcs::pair_counts_score(&source_counts)
+                    + Alcs::pair_counts_score(&target_counts);
                 if new_pair_scores < pair_scores {
                     pair_offsets = (source_offset, target_offset);
                     pair_scores = new_pair_scores;
@@ -89,20 +108,46 @@ mod compare_counts_tests {
 
     #[test]
     fn test_pair_damage() {
-        let source = [4, 3, 4, 3];
+        let source =  [2, 1];
         dbg!(Alcs::pair_counts_damage(&source));
     }
 
     #[test]
     fn test_pair_score() {
-        let source = [4, 3, 4, 3];
+        let source =  [2, 1];
         dbg!(Alcs::pair_counts_score(&source));
     }
 
     #[test]
     fn test_next_pair_offsets() {
-        let source = [6, 9, 7, 2, 6, 2, 4, 2, 3, 3, 8, 9, 4, 8, 9, 7, 6, 8, 5, 7, 6, 0, 7, 3, 4, 4];
-        let target = [0, 2, 9, 8, 3, 7, 5, 6, 8, 3, 6, 3, 5, 1, 4, 0, 7, 4, 1, 9, 5, 7, 5, 8];
+        let source = [
+            6, 9, 7, 2, 6, 2, 4, 2, 3, 3, 8, 9, 4, 8, 9, 7, 6, 8, 5, 7, 6, 0, 7, 3, 4, 4,
+        ];
+        let target = [
+            0, 2, 9, 8, 3, 7, 5, 6, 8, 3, 6, 3, 5, 1, 4, 0, 7, 4, 1, 9, 5, 7, 5, 8,
+        ];
         dbg!(Alcs::new(&source, &target).next_pair_offsets(&source, &target));
+    }
+
+    #[test]
+    fn test_subsequence() {
+        let source = [
+            1, 6, 9, 7, 2, 6, 2, 4, 2, 3, 3, 8, 9, 4, 8, 9, 7, 6, 8, 5, 7, 6, 0, 7, 3, 4, 4,
+        ];
+        let target = [
+            8, 5, 1, 0, 2, 9, 8, 3, 7, 5, 6, 8, 3, 6, 3, 5, 1, 4, 0, 7, 4, 1, 9, 5, 7, 5, 8,
+        ];
+        dbg!(Alcs::new(&source, &target).subsequence());
+    }
+
+    #[test]
+    fn test_len() {
+        let source = [
+            1, 6, 9, 7, 2, 6, 2, 4, 2, 3, 3, 8, 9, 4, 8, 9, 7, 6, 8, 5, 7, 6, 0, 7, 3, 4, 4,
+        ];
+        let target = [
+            8, 5, 1, 0, 2, 9, 8, 3, 7, 5, 6, 8, 3, 6, 3, 5, 1, 4, 0, 7, 4, 1, 9, 5, 7, 5, 8,
+        ];
+        dbg!(Alcs::new(&source, &target).len());
     }
 }
