01ce4b4f80567f2f13de95e5f9169619466531ff
Navoso <VantorreWannes@gmail.com>
2024-04-07 16:22:51 +0200

    Cleanup old files

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,7 +1,3 @@
-pub mod closest_offset_sum_lcs;
-pub mod counts_lcs;
-pub mod slow_lcs;
-
 fn main() {
     divan::main();
 }
--- src/new_algos/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod closest_offset_sum_lcs;
-pub mod counts_lcs;
-
-pub mod min_count_lcs;
