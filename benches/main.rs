5a53da39ba8e9bacdfac833d87a5c6613876abdf
Wannes Vantorre <vantorrewannes@gmail.com>
2024-09-10 13:21:32 +0200

    feature: Add usefull utility benchmarks
    - Add usefull utility benchmarks

--- benches/main.rs
+++ benches/main.rs
@@ -1,6 +1,7 @@
 pub mod closest_offset_sum_lcs;
 pub mod slow_lcs;
 pub mod tracking_offset_sum_lcs;
+pub mod utilities;
 
 fn main() {
     divan::main();
---
+++ benches/utilities/mod.rs
@@ -0,0 +1,28 @@
+use divan::Bencher;
+use fast_lcs::utilities::{counts, filter_shared, indexes};
+use rand::{distributions::Uniform, prelude::Distribution};
+
+#[divan::bench]
+fn counts_u8(bencher: Bencher) {
+    let mut rng = rand::thread_rng();
+    let die: Uniform<u8> = Uniform::from(0..=255);
+    let slice: Vec<u8> = die.sample_iter(&mut rng).take(1000).collect();
+    bencher.bench_local(move || counts(&slice));
+}
+
+#[divan::bench]
+fn indexes_u8(bencher: Bencher) {
+    let mut rng = rand::thread_rng();
+    let die: Uniform<u8> = Uniform::from(0..=255);
+    let slice: Vec<u8> = die.sample_iter(&mut rng).take(1000).collect();
+    bencher.bench_local(move || indexes(&slice));
+}
+
+#[divan::bench]
+fn filter_shared_usize(bencher: Bencher) {
+    let mut rng = rand::thread_rng();
+    let die: Uniform<usize> = Uniform::from(0..=10000);
+    let slice: Vec<usize> = die.sample_iter(&mut rng).take(10000).collect();
+    let other: Vec<usize> = die.sample_iter(&mut rng).take(10000).collect();
+    bencher.bench_local(move || filter_shared(&slice, &other));
+}
